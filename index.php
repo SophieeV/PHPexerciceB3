95d69da32b479c95c7421c10f8f77a408941eff6
SophieeV <sophievesin@wanadoo.fr>
2021-06-14 11:51:50 +0200

    ajout fihcier

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+$number = 0;
+
+for ($number=0; $number<=15;$number++)
+{
+    echo $number;
+    if ($number <15){
+        echo ': on y est presque <br>';
+    }else{
+        echo ': c\'est bon';
+    }
+
+
+}
+
+?>
